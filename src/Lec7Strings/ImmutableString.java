74d71f744dc25fd086c0ad1fc8f2d078f5e23bfa
hoanghoang <hoangngochoang0508@gmail.com>
2025-08-28 16:42:12 +0700

    first commit

---
+++ src/Lec7Strings/ImmutableString.java
@@ -0,0 +1,11 @@
+
+package Lec7Strings;
+
+public class ImmutableString {
+    public static void main(String[] args) {
+        String s1 = "Duy Tan";
+        String s2 = s1.concat("University");
+        System.out.println(s1);
+    }
+ 
+}
---
+++ src/Lec7Strings/ImmutableStringEx.java
@@ -0,0 +1,10 @@
+
+package Lec7Strings;
+
+public class ImmutableStringEx {
+    public static void main(String[] args) {
+        String s = "Duy Tan ";
+        s = s.concat("University");
+        System.out.println(s);
+    }
+}
